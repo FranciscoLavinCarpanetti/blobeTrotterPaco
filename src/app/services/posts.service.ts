09b83554260f08a933df7f4694515fdbf128d488
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-31 20:55:16 +0100

    trabajo en componente footer

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { IPost } from '../interfaces/ipost.interface';
+import { POSTS } from '../db/post.db';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  private posts: IPost[] = POSTS;
+
+
+  getAll(): IPost[] {
+    return this.posts;
+  }
+
+  insert(post: IPost): void {
+    this.posts.push(post);
+  }
+
+  getByCategory(category: string): IPost[] {
+    return this.posts.filter(post => post.category === category);
+  }
+
+  getById(id: number): IPost | undefined {
+    return this.posts.find(post => post.id === id);
+  }
+
+}
