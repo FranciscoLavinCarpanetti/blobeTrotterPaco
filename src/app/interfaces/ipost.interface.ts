236e530617a7b5f636c112435dafb551f77e6d65
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-31 19:24:57 +0100

    creación de interface IPost.

---
+++ src/app/interfaces/ipost.interface.ts
@@ -0,0 +1,12 @@
+import { ICategory } from './icategory.interface';
+
+export interface IPost {
+  id?: number;
+  title: string;
+  text: string;
+  author: string;
+  image: string;
+  date: string;
+  category: ICategory;
+}
+
