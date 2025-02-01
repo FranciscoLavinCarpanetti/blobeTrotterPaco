93650dddfc045ef4849dab6bcb4f08e73131db45
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-01 17:33:05 +0100

    arreglo del routerLink import

--- src/app/pages/post-list/post-list.component.ts
+++ src/app/pages/post-list/post-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { PostsService } from './../../services/posts.service';
+import { Component, inject } from '@angular/core';
+import { IPost } from '../../interfaces/ipost.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
+  arrposts: IPost[] = [];
+  PostsService = inject(PostsService)
+
+
+
+
+  ngOnInit() {
+    this.arrposts = this.PostsService.getAll()
+  }
+
+
+
 
 }
