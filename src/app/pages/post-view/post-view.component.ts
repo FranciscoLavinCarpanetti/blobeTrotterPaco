afcd1cb4da11765631c46e14a6358458df23188f
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-01 17:33:42 +0100

    arreglo de visual de post-view

--- src/app/pages/post-view/post-view.component.ts
+++ src/app/pages/post-view/post-view.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { IPost } from './../../interfaces/ipost.interface';
+import { PostsService } from './../../services/posts.service';
+import { Component, inject, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-post-view',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-view.component.html',
   styleUrl: './post-view.component.css'
 })
 export class PostViewComponent {
+  @Input() idPost: string = '';
+  PostsService = inject(PostsService)
+  router = inject(Router)
+  usuario!: IPost;
+
+
+
+
+
+  selectPost(id: number) {
+    this.router.navigate(['/post', id])
+  }
 
 }
+
