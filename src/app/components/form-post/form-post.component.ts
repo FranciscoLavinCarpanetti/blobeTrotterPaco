c508121487ffd87306a1c744e7ce094a99c68f3f
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-01 11:55:20 +0100

    components para la web

---
+++ src/app/components/form-post/form-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-post',
+  imports: [],
+  templateUrl: './form-post.component.html',
+  styleUrl: './form-post.component.css'
+})
+export class FormPostComponent {
+
+}
