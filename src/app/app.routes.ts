41858b1aed7219669a74f360a8bf685f1ea682fb
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-01 11:54:38 +0100

    se cargaron las rutas de enlace en la web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './pages/post-list/post-list.component';
+import { FormPostComponent } from './components/form-post/form-post.component';
+import { PostViewComponent } from './pages/post-view/post-view.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: PostListComponent },
+  { path: 'new', component: FormPostComponent },
+  { path: 'post/:id', component: PostViewComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'home' }
+];
