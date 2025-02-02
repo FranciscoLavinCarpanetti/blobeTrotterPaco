60cd6e69f3a63be0b4c023b86858605d9b0e9cc9
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-02 09:35:43 +0100

    se realizo formulario de new pots,no logro que funcione.

--- src/app/components/form-post/form-post.component.ts
+++ src/app/components/form-post/form-post.component.ts
@@ -1,11 +1,39 @@
+import { PostsService } from './../../services/posts.service';
 import { Component } from '@angular/core';
+import { IPost } from '../../interfaces/ipost.interface';
+import { inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-post',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-post.component.html',
-  styleUrl: './form-post.component.css'
+  styleUrls: ['./form-post.component.css']
 })
 export class FormPostComponent {
+  arrPost: IPost[] = [];
+  PostsService = inject(PostsService, { self: true });
+  postForm: FormGroup;
 
+  constructor() {
+    this.postForm = new FormGroup({
+      nombre: new FormControl('', Validators.required),
+      apellido: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      telefono: new FormControl('', Validators.required),
+      departamento: new FormControl('', Validators.required),
+      salario: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit(): void {
+    if (this.postForm?.valid) {
+      const formData = this.postForm.value;
+      this.PostsService.createPost(formData);
+      this.arrPost.push(formData);
+      this.postForm.reset();
+    } else {
+      alert('Please fill out all required fields.');
+    }
+  }
 }
