8bb1a70ae7730a07a0716543dbc26d4eeca482e7
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-02 11:46:09 +0100

    se acomodaron las opciones de categoría, input de imagen y el botón de enviar

--- src/app/components/form-post/form-post.component.ts
+++ src/app/components/form-post/form-post.component.ts
@@ -1,8 +1,9 @@
-import { PostsService } from './../../services/posts.service';
+
 import { Component } from '@angular/core';
 import { IPost } from '../../interfaces/ipost.interface';
-import { inject } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form-post',
@@ -12,10 +13,39 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
 })
 export class FormPostComponent {
   arrPost: IPost[] = [];
-  PostsService = inject(PostsService, { self: true });
-  postForm: FormGroup;
 
-  constructor() {
+  postForm: FormGroup = new FormGroup({});
+
+  // //constructor() {
+  //   this.postForm = new FormGroup({
+  //     nombre: new FormControl('', Validators.required),
+  //     apellido: new FormControl('', Validators.required),
+  //     email: new FormControl('', [Validators.required, Validators.email]),
+  //     telefono: new FormControl('', Validators.required),
+  //     departamento: new FormControl('', Validators.required),
+  //     salario: new FormControl('', Validators.required),
+  //   });
+  // }
+
+  // onSubmit(): void {
+  //   if (this.postForm?.valid) {
+  //     const formData = this.postForm.value;
+  //     this.PostsService.createPost(formData);
+  //     this.arrPost.push(formData);
+  //     this.postForm.reset();
+  //   } else {
+  //     alert('Please fill out all required fields.');
+  //   }
+  // }
+
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
     this.postForm = new FormGroup({
       nombre: new FormControl('', Validators.required),
       apellido: new FormControl('', Validators.required),
@@ -27,13 +57,10 @@ export class FormPostComponent {
   }
 
   onSubmit(): void {
-    if (this.postForm?.valid) {
-      const formData = this.postForm.value;
-      this.PostsService.createPost(formData);
-      this.arrPost.push(formData);
-      this.postForm.reset();
+    if (this.postForm.valid) {
+      console.log('Formulario enviado:', this.postForm.value);
     } else {
-      alert('Please fill out all required fields.');
+      console.log('Formulario inválido');
     }
   }
 }
