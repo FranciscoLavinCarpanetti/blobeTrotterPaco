c70e482ac8be918aec6244a99842f4218d3eba0d
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-02 18:47:48 +0100

    se trabajo en html y css de registro.

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { PostsService } from './../../services/posts.service';
+
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  registroForm: FormGroup;
+  PostsService = inject(PostsService)
+
+
+
+  constructor() {
+    this.registroForm = new FormGroup({
+      username: new FormControl('', []),
+      email: new FormControl('', []),
+      password: new FormControl('', []),
+    }, []);
+  }
+
 
 }
