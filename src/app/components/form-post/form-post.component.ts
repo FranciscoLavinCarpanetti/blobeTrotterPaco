1413f63aae2bd2fe27af5bfd49a8c353d788dcf7
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-04 20:21:35 +0100

    funcionalidad para crear correctamente en el array un nuevo post.  ademas se cargo alerta de post creqdo correctamente con sweetAlert2.

--- src/app/components/form-post/form-post.component.ts
+++ src/app/components/form-post/form-post.component.ts
@@ -1,8 +1,9 @@
+import { PostsService } from './../../services/posts.service';
+import { IPost } from './../../interfaces/ipost.interface';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2'
 
-import { Component } from '@angular/core';
-import { IPost } from '../../interfaces/ipost.interface';
-
-import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -16,51 +17,95 @@ export class FormPostComponent {
 
   postForm: FormGroup = new FormGroup({});
 
-  // //constructor() {
-  //   this.postForm = new FormGroup({
-  //     nombre: new FormControl('', Validators.required),
-  //     apellido: new FormControl('', Validators.required),
-  //     email: new FormControl('', [Validators.required, Validators.email]),
-  //     telefono: new FormControl('', Validators.required),
-  //     departamento: new FormControl('', Validators.required),
-  //     salario: new FormControl('', Validators.required),
-  //   });
-  // }
+  PostsService = inject(PostsService)
 
-  // onSubmit(): void {
-  //   if (this.postForm?.valid) {
-  //     const formData = this.postForm.value;
-  //     this.PostsService.createPost(formData);
-  //     this.arrPost.push(formData);
-  //     this.postForm.reset();
-  //   } else {
-  //     alert('Please fill out all required fields.');
-  //   }
-  // }
-
-
-
-
-
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
     this.postForm = new FormGroup({
-      nombre: new FormControl('', Validators.required),
-      apellido: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      telefono: new FormControl('', Validators.required),
-      departamento: new FormControl('', Validators.required),
-      salario: new FormControl('', Validators.required),
+      title: new FormControl("", [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60),
+      ]),
+      text: new FormControl("", [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(300),
+      ]),
+      author: new FormControl("", [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60),
+      ]),
+      image: new FormControl("", [
+        Validators.required,
+        Validators.pattern(/^.*\.(jpg|jpeg|png|gif)$/)
+      ]),
+      date: new FormControl("", [
+        Validators.required,
+      ]),
+      category: new FormControl("", [
+        Validators.required,])
     });
+
   }
 
-  onSubmit(): void {
+  getDataForm() {
     if (this.postForm.valid) {
-      console.log('Formulario enviado:', this.postForm.value);
+      const existingPosts = this.PostsService.getAll();
+      const lastId = existingPosts.length > 0
+        ? Math.max(...existingPosts.map(post => post.id).filter(id => id !== undefined))
+        : 0;
+
+      const newPost: IPost = {
+        id: lastId + 1,
+        title: this.postForm.value.title,
+        text: this.postForm.value.text,
+        author: this.postForm.value.author, // Corregido de "author"
+        image: this.postForm.value.image,
+        date: this.postForm.value.date, // Corregido de "date"
+        category: this.postForm.value.category
+      };
+
+      this.PostsService.createPost(newPost);
+
+
+      console.log('Nuevo post agregado:', newPost);
+      console.log('Lista de posts actualizada:', this.PostsService.getAll());
+      Swal.fire({
+        title: "¿Quieres crear el post?",
+        text: "¡No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "¡Crear Post!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "¡Post creado!",
+            text: "Te confirmamos que tu post ha sido creado.",
+            icon: "success"
+          });
+        }
+      });
+
+      this.postForm.reset();
     } else {
-      console.log('Formulario inválido');
+      console.warn('El formulario tiene errores, no se puede agregar el post.');
     }
   }
+
+
+
+
+
+
+  chechErrorField(field: string, err: string): boolean {
+    if (this.postForm.get(field)?.hasError(err) && this.postForm.get(field)?.touched) {
+      return true;
+    }
+    return false;
+  }
+
 }
+
